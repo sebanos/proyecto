3ed7584d8afe3e939d9d37c78b2340fe7a34379f
usuar1o <juansosma@gmail.com>
2025-03-20 19:32:08 -0500

    Implementacion de funciones CRUD en SQLite3

--- archivo1.py
+++ archivo1.py
@@ -58,3 +58,102 @@ def mostrar_usuarios():
     conn.close()
 
 mostrar_usuarios()
+
+def create_user(nombre, telefono, email, ciudad, direccion):
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO usuarios (nombre, telefono, email, ciudad, direccion) VALUES (?, ?, ?, ?, ?)", 
+                   (nombre, telefono, email, ciudad, direccion))
+
+    conn.commit()
+    conn.close()
+
+    print(f"Usuario {nombre} agregado correctamente.")
+
+
+
+def read_users():
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM usuarios")
+    usuarios = cursor.fetchall()
+
+    conn.close()
+
+    if usuarios:
+        print("Lista de usuarios:")
+        for usuario in usuarios:
+            print(usuario)
+    else:
+        print("No hay usuarios en la base de datos.")
+
+
+
+
+def update_phone(user_id, new_phone):
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE usuarios SET telefono = ? WHERE id = ?", (new_phone, user_id))
+
+    if cursor.rowcount > 0:
+        print(f"Teléfono actualizado correctamente para el usuario con ID {user_id}.")
+    else:
+        print(f"No se encontró el usuario con ID {user_id}.")
+
+    conn.commit()
+    conn.close()
+
+
+
+def update_email(user_id, new_email):
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE usuarios SET email = ? WHERE id = ?", (new_email, user_id))
+
+    if cursor.rowcount > 0:
+        print(f"Email actualizado correctamente para el usuario con ID {user_id}.")
+    else:
+        print(f"No se encontró el usuario con ID {user_id}.")
+
+    conn.commit()
+    conn.close()
+
+
+def update_full(user_id, new_phone, new_email, new_city, new_address):
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        UPDATE usuarios 
+        SET telefono = ?, email = ?, ciudad = ?, direccion = ? 
+        WHERE id = ?
+    """, (new_phone, new_email, new_city, new_address, user_id))
+
+    if cursor.rowcount > 0:
+        print(f"Datos actualizados correctamente para el usuario con ID {user_id}.")
+    else:
+        print(f"No se encontró el usuario con ID {user_id}.")
+
+    conn.commit()
+    conn.close()
+
+def delete_user(user_id):
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM usuarios WHERE id = ?", (user_id,))
+
+    if cursor.rowcount > 0:
+        print(f"Usuario con ID {user_id} eliminado correctamente.")
+    else:
+        print(f"No se encontró el usuario con ID {user_id}.")
+
+    conn.commit()
+    conn.close()
+
+
+
